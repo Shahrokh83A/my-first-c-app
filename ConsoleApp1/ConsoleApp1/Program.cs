ec599867d3a6adc62fd391460ebd44373dc4aa55
Shahrukh <shahrokh.ab.83@gmail.com>
2024-09-11 14:57:56 +0330

    code aded

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace محاسبات_ریاضی_با_سیشارپ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // متغییر ها
+            double Pi = Math.PI;
+            double ghotr;
+            double mohit;
+            double masahat;
+            double voroodi;
+            // شرط ها
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("                                  mohasebat DAYERE                                       ");
+            Console.WriteLine();
+            // تابع
+            void mohasebe(double shoa)
+            {
+                ghotr = shoa * 2;
+                mohit = ghotr * Pi;
+                masahat = (shoa * shoa) * Pi;
+                Console.Write("ghotr = ");
+                Console.WriteLine(ghotr);
+                Console.Write("mohite = ");
+                Console.WriteLine(mohit);
+                Console.Write("masahat = ");
+                Console.WriteLine(masahat);
+                ghotr = 0; masahat = 0; mohit = 0;
+                Console.WriteLine();
+            }
+            // حلقه
+            while (true)
+            {
+                try
+                {
+
+                    while (true)
+                    {
+                        Console.WriteLine("shoaye dayere khod ra vared konid : ");
+                        voroodi = Convert.ToDouble(Console.ReadLine());
+                        if (voroodi < 0)
+                        {
+                            Console.WriteLine("lotfan adad mosbat vared konid!!!!\n");
+                            break;
+                        }
+                        mohasebe(voroodi);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("lotfan adad vared konid !!!");
+                }
+            }
+        }
+    }
+}
